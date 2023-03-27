32e335d067d36caa6cf7a40fc89a63de37d1727d
FranklinThree <1765370549@qq.com>
2023-03-27 13:33:35 +0800

    049-Bean的生命周期之五步

---
+++ spring6-006-bean-lifecycle/src/test/java/com/github/franklinthree/spring6/test/BeanLifecycleTest.java
@@ -0,0 +1,19 @@
+package com.github.franklinthree.spring6.test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.github.franklinthree.spring6.bean.User;
+
+public class BeanLifecycleTest {
+
+    @Test
+    public void testBeanLifecycleFive(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        User user = applicationContext.getBean("user", User.class);
+        System.out.println("第四步：使用Bean："+user);
+        // 注意：这里需要手动关闭容器，否则不会调用Bean的销毁方法。
+        ClassPathXmlApplicationContext context = (ClassPathXmlApplicationContext) applicationContext;
+        context.close();
+    }
+}
