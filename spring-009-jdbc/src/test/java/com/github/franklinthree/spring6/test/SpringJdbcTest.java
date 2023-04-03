2bca30db269b1135e96c03faac4c0bbec60a4815
FranklinThree <1765370549@qq.com>
2023-04-03 13:44:37 +0800

    082-JdbcTemplate之新增

--- spring-009-jdbc/src/test/java/com/github/franklinthree/spring6/test/SpringJdbcTest.java
+++ spring-009-jdbc/src/test/java/com/github/franklinthree/spring6/test/SpringJdbcTest.java
@@ -15,10 +15,26 @@ import org.springframework.jdbc.core.JdbcTemplate;
  * @since 1.0.0
  */
 public class SpringJdbcTest {
+    private final JdbcTemplate jdbcTemplate;
+    public SpringJdbcTest() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring6.xml");
+        JdbcTemplate template = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        System.out.println(template);
+        jdbcTemplate = template;
+    }
+    @Test
+    public void testInsert(){
+        // insert语句
+        String sql = "insert into t_user(real_name, age) values(?, ?)";
+        // 注意：在JdbcTemplate中，只要是insert delete update都是用update方法。
+        int count = jdbcTemplate.update(sql, "阿三", 3);
+        System.out.println(count);
+    }
+
     @Test
     public void testJdbc(){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring6.xml");
-        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
-        System.out.println(jdbcTemplate);
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring6.xml");
+//        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+//        System.out.println(jdbcTemplate);
     }
 }
