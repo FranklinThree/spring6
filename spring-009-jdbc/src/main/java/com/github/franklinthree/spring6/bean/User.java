0bac0c60fc6f1350f19f7e26cb9725fd69ed1621
FranklinThree <1765370549@qq.com>
2023-04-03 13:53:53 +0800

    084-JdbcTemplate之查一个对象

--- spring-009-jdbc/src/main/java/com/github/franklinthree/spring6/bean/User.java
+++ spring-009-jdbc/src/main/java/com/github/franklinthree/spring6/bean/User.java
@@ -14,9 +14,24 @@ public class User {
     private String realName;
     private Integer age;
 
+    public User() {
+    }
+
     public User(Integer id, String realName, Integer age) {
         this.id = id;
         this.realName = realName;
+        this.age = age;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public void setAge(Integer age) {
         this.age = age;
     }
 
