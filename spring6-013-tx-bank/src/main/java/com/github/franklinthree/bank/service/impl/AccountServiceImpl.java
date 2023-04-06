fd6d5347424884574f381abc8c9e964d8d7ec66b
FranklinThree <1765370549@qq.com>
2023-04-06 17:18:55 +0800

    122-Spring事务之注解方式

--- spring6-013-tx-bank/src/main/java/com/github/franklinthree/bank/service/impl/AccountServiceImpl.java
+++ spring6-013-tx-bank/src/main/java/com/github/franklinthree/bank/service/impl/AccountServiceImpl.java
@@ -5,6 +5,7 @@ import com.github.franklinthree.bank.pojo.Account;
 import com.github.franklinthree.bank.service.AccountService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 账户服务实现
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
  * @since 1.0.0
  */
 @Service("accountService")
+@Transactional
 public class AccountServiceImpl implements AccountService {
     @Resource
     private AccountDao accountDao;
@@ -48,8 +50,8 @@ public class AccountServiceImpl implements AccountService {
 
         // 更新数据库
         int outCount = accountDao.update(outAccount);
-        // 模拟异常
-        int i = 1 / 0;
+//        // 模拟异常
+//        int i = 1 / 0;
 
         int inCount = accountDao.update(inAccount);
 
